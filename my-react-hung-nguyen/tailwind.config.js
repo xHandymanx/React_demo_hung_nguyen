cb61bcb2d5da4df520c3cc1ed69967ad6a60d905
Hung Nguyen <fhung004@edu.xamk.fi>
2025-01-21 23:26:43 +0200

    first version

---
+++ my-react-hung-nguyen/tailwind.config.js
@@ -0,0 +1,23 @@
+/** @type {import('tailwindcss').Config} */
+export default {
+  content: [],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: ["./src/**/*.{html,js}"],
+  theme: {
+    extend: {},
+  },
+  plugins: [],
+}
+module.exports = {
+  //...
+  plugins: [
+    require('daisyui'),
+  ],
+}
+
